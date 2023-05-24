ce510ac41f9902175be76a4127394cfff74695f6
bheki97 <bfmokoena7@gmail.com>
2023-05-24 13:00:32 +0200

    Working on the branch using git bash

--- test-project/src/main/java/za/co/mie/techgiants/testproject/model/MyModel.java
+++ test-project/src/main/java/za/co/mie/techgiants/testproject/model/MyModel.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package za.co.mie.techgiants.testproject.model;
 
-/**
- *
- * @author STUDIO 18
- */
 public class MyModel {
     
     
