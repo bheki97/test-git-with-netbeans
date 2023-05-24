030fe9cba97cd51568ebc137b73039a4165893b1
bheki97 <bfmokoena7@gmail.com>
2023-05-24 08:27:08 +0200

    initial commit

---
+++ test-project/src/main/java/za/co/mie/techgiants/testproject/model/MyModel.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.co.mie.techgiants.testproject.model;
+
+/**
+ *
+ * @author STUDIO 18
+ */
+public class MyModel {
+    
+    
+    private String model;
+    private String type;
+
+    public MyModel(String model, String type) {
+        this.model = model;
+        this.type = type;
+    }
+
+    public MyModel() {
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "MyModel{" + "model=" + model + ", type=" + type + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + (this.model != null ? this.model.hashCode() : 0);
+        hash = 47 * hash + (this.type != null ? this.type.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MyModel other = (MyModel) obj;
+        if ((this.model == null) ? (other.model != null) : !this.model.equals(other.model)) {
+            return false;
+        }
+        if ((this.type == null) ? (other.type != null) : !this.type.equals(other.type)) {
+            return false;
+        }
+        return true;
+    }
+
+
+    
+    
+}
